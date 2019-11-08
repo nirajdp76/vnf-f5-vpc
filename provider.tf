591764e36c58b71a9cad150882086bfb64d7595f
Niraj Patel <nirajdp@us.ibm.com>
2019-11-08 10:36:42 -0600

    feat(poc): initial terraform templates.

--- provider.tf
+++ provider.tf
@@ -3,8 +3,14 @@ variable "region" {
   description = "The VPC Region that you want your VPC, networks and the F5 virtual server to be provisioned in. To list available regions, run `ibmcloud is regions`."
 }
 
+variable "riaas_endpoint" {
+  default = "us-south.iaas.cloud.ibm.com"
+  description = "The VPC Regional api endpoint. To list available regional endpoints, run `ibmcloud is regions`."
+}
+
 provider "ibm" {
   generation            = 2
   region                = "${var.region}"
   ibmcloud_timeout      = 300
+  riaas_endpoint        = "${var.riaas_endpoint}"
 }
