b2c8ea46155866d9b0132bc6816a03ca293d7060
Niraj Patel <nirajdp@us.ibm.com>
2019-11-10 14:24:33 -0600

    fix(poc): make terraform work against vpc staging endpoint.

--- wrapper_script.sh
+++ wrapper_script.sh
@@ -7,7 +7,7 @@ export IBMCLOUD_IS_API_ENDPOINT=us-south-stage01.iaasdev.cloud.ibm.com
 printenv
 
 terraform init
-terraform apply -auto-approve -var-file=schematics.tfvars
+terraform apply -auto-approve -state=terraform.tfstate -var-file=schematics.tfvars
 
 # hack to show the VPC resource on the schematics UI, but destroy will not work
 #cp -f terraform.tfstate ../
